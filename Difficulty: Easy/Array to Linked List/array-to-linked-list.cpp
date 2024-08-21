1fd78549397f7ebad3598017fa0805c64df18fb4
M-SRIKAR-VARDHAN <121815832+M-SRIKAR-VARDHAN@users.noreply.github.com>
2024-08-21 17:55:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*class Node {
+public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        Node* head = new Node(arr[0]);
+        Node* temp=head;
+        int size1=arr.size();
+        for(int i=1;i<size1;i++){
+            Node* temp1=new Node(arr[i]);
+            temp->next=temp1;
+            temp=temp1;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        Solution ob;
+        Node* ans = ob.constructLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
