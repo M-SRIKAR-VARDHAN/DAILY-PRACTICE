52046beb07e6680f13574829fcab148d3dcde018
M-SRIKAR-VARDHAN <121815832+M-SRIKAR-VARDHAN@users.noreply.github.com>
2024-08-22 22:39:40 +0530

    Time: 13 ms (13.58%), Space: 15.6 MB (8.24%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,20 +11,41 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (head != NULL) {
-    
-            ListNode* odd = head;
-            ListNode* even = head->next;
-            ListNode* evenHead = even; 
-        
-            while (even != NULL && even->next != NULL) {
-                odd->next = odd->next->next; 
-                even->next = even->next->next; 
-                odd = odd->next;
-                even = even->next;
+         if (!head || !head->next) return head;
+
+        std::queue<int> oddQueue;
+        std::queue<int> evenQueue;
+
+        ListNode* temp = head;
+        int index = 1;
+
+        // Separate nodes into odd and even queues
+        while (temp != nullptr) {
+            if (index % 2 != 0) {
+                oddQueue.push(temp->val);
+            } else {
+                evenQueue.push(temp->val);
             }
-            odd->next = evenHead; 
+            temp = temp->next;
+            index++;
         }
-        return head;
+
+        temp = head;
+
+        // Reassign nodes from the odd queue
+        while (!oddQueue.empty()) {
+            temp->val = oddQueue.front();
+            oddQueue.pop();
+            temp = temp->next;
+        }
+
+        // Reassign nodes from the even queue
+        while (!evenQueue.empty()) {
+            temp->val = evenQueue.front();
+            evenQueue.pop();
+            temp = temp->next;
+        }
+
+        return head; 
     }
 };
