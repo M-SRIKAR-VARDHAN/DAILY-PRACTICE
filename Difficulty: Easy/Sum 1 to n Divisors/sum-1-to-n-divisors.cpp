fe04c5d0bba1fdce6b8708c01dc8ac4347b9e4ee
M-SRIKAR-VARDHAN <121815832+M-SRIKAR-VARDHAN@users.noreply.github.com>
2025-01-17 15:38:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum 1 to n Divisors/sum-1-to-n-divisors.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int sumOfDivisors(int n) {
+         int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i * (n / i); // Add i * (number of multiples of i)
+        }
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans = ob.sumOfDivisors(N);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
