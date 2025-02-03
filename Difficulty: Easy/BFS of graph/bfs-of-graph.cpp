21ba379c472a526791dd78ce9942f324bdc49d30
M-SRIKAR-VARDHAN <121815832+M-SRIKAR-VARDHAN@users.noreply.github.com>
2025-02-03 16:26:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+   
+    vector<int> bfsOfGraph(vector<vector<int>> &adj) {
+       vector<int> ans;
+       int size=adj.size();
+       vector<bool> vis(size,false);
+       queue<int> q;
+       q.push(0);
+       vis[0]=true;
+       while(!q.empty())
+       {
+       int front=q.front();
+       q.pop();
+       ans.push_back(front);
+       
+       for(auto &it: adj[front]){
+           if(!vis[it]){
+               q.push(it);
+               vis[it]=true;
+           }
+       }
+       }
+       
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        // Now using vector of vectors instead of array of vectors
+        vector<vector<int>> adj(V);
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u); // For undirected graph, add both u->v and v->u
+        }
+
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
